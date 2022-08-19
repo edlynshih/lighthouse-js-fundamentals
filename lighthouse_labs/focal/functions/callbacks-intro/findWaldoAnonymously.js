3750058b4ce60a4764f7366e36b63dce2916ec4c
Edlyn Shih <102762089+edlynshih@users.noreply.github.com>
2022-08-19 17:49:44 -0400

    Delete lighthouse_labs/focal/functions/callbacks-intro directory

--- lighthouse_labs/focal/functions/callbacks-intro/findWaldoAnonymously.js
+++
@@ -1,9 +0,0 @@
-const findWaldo = (names, found) => {
-  names.forEach(function(name, index) {
-    if (name === "Waldo") {
-      found(name, index);   // execute callback
-    }
-  })
-};
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], (name, index) => console.log(`Found ${name} at index ${index}!`));
